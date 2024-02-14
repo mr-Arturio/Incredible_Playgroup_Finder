39db3cacd0482830b7991c80ddf6add6f4dd71ad
ArturT <mr.arturio@gmail.com>
2024-02-14 00:16:25 -0500

    add map component

---
+++ src/components/MapComponent.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MapComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''
+  });
+
+  return isLoaded ? (
+    // Wrap the GoogleMap component in a div styled with Tailwind CSS
+    <div className="w-full md:w-96 h-96"> {/* Adjust width and height as needed */}
+      <GoogleMap
+        mapContainerStyle={{ width: '100%', height: '100%' }} // Make the map fill the container
+        center={center}
+        zoom={10}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+      </GoogleMap>
+    </div>
+  ) : <></>;
+}
+
+export default React.memo(MapComponent);
