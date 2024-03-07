b6939b7a951509533056a4f9678253af5ff53d80
ArturT <mr.arturio@gmail.com>
2024-03-06 23:36:41 -0500

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+
+  return (
+    <footer>
+      <p> © 2024  Project - Built by <a href="https://github.com/mr-Arturio"> Artur</a> </p>
+    </footer>
+  );
+};
