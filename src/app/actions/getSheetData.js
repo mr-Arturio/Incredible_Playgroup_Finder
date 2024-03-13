89fa0b07301c54e9c49bf47bc4b528a3c37b13a5
ArturT <mr.arturio@gmail.com>
2024-03-13 19:39:20 -0400

    move create a renderData file and move all logixc there

--- src/app/actions/getSheetData.js
+++ src/app/actions/getSheetData.js
@@ -1,34 +1,30 @@
-'use server';
+"use server";
 import { google } from "googleapis";
 
-export async function getSheetData() { 
+export async function getSheetData() {
   const glAuth = await google.auth.getClient({
-        projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
-        credentials:{
-            type: "service_account",
-            private_key:  process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-            client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-          },
-        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    });
+    projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
+    credentials: {
+      type: "service_account",
+      private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+      client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
 
-    const glSheets = google.sheets({ version: "v4", auth: glAuth });
+  const glSheets = google.sheets({ version: "v4", auth: glAuth });
 
-    const data = await glSheets.spreadsheets.values.get({
-        spreadsheetId: process.env.GOOGLE_SHEET_ID,
-        range: 'A1:A',
-    });
+  const data = await glSheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range: "A1:M",
+  });
 
-    const date = data.data.values;
-console.log(date);
+  const date = data.data.values;
+  console.log(date);
 
-
-return { 
-  props: {
-    sheetData: date
-    
-  } 
+  return {
+    props: {
+      sheetData: date,
+    },
+  };
 }
-    
-}
-
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import React, { useEffect, useState } from "react";
 import Navbar from "@/app/components/Navbar";
 import { Footer } from "@/app/components/Footer";
 import MapComponent from "@/app/components/MapComponent";
-import { getSheetData } from "@/app/actions/getSheetData";  
+import { getSheetData } from "@/app/actions/getSheetData";
+import RenderSheetDataTable from "@/app/components/RenderSheetDataTable";
+
 
 function Home() {
   const [sheetData, setSheetData] = useState(null);
@@ -12,29 +14,21 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       const response = await getSheetData();
-      // Directly use the response assuming it's structured as expected
-      // Adjust this line according to the actual structure of the response
       setSheetData(response.props.sheetData); // Assuming the data is structured correctly
     };
     fetchData();
   }, []);
 
-  const renderSheetData = () => {
-    if (!sheetData) return <p>No data available</p>;
-    // Ensure sheetData is iterable. Adjust as needed based on the actual data structure.
-    return sheetData.map((data, index) => <div key={index}>{data}</div>);
-  };
-
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm ">
           <h1>Welcome to Our Application</h1>
           <MapComponent />
           <article>
             <h1>The data</h1>
-            <div>{renderSheetData()}</div>
+            <div><RenderSheetDataTable sheetData={sheetData} /></div>
           </article>
         </div>
       </main>
