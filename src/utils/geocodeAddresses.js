35fb88968587b6f6fa2813417dfafea173fdd83b
ArturT <mr.arturio@gmail.com>
2024-03-19 14:21:22 -0400

    store geamarkers in local storage cache, show pins withouts dubles

--- src/utils/geocodeAddresses.js
+++ src/utils/geocodeAddresses.js
@@ -1,31 +1,41 @@
-const geocodeAddresses = async (addresses, apiKey) => {
-  const promises = addresses.map((address) =>
-    fetch(
+const geocodeAddress = async (address, apiKey) => {
+  const cachedResult = localStorage.getItem(address);
+  if (cachedResult) {
+    return JSON.parse(cachedResult);
+  } else {
+    const response = await fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
         address
       )}&key=${apiKey}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.results && data.results.length > 0) {
-          const location = data.results[0].geometry.location;
-          return {
-            lat: location.lat,
-            lng: location.lng,
-          };
-        } else {
-          throw new Error("Geocoding failed for address: " + address);
-        }
-      })
-  );
+    );
+    const data = await response.json();
+    if (data.results && data.results.length > 0) {
+      const location = data.results[0].geometry.location;
+      const marker = { lat: location.lat, lng: location.lng };
+      localStorage.setItem(address, JSON.stringify(marker));
+      return marker;
+    } else {
+      throw new Error("Geocoding failed for address: " + address);
+    }
+  }
+};
 
+const geocodeAddresses = async (addresses, apiKey) => {
+  // Create a Set from addresses to filter out duplicates, then convert back to an array
+  const uniqueAddresses = Array.from(new Set(addresses));
   try {
-    const geocodedMarkers = await Promise.all(promises);
+    const geocodePromises = uniqueAddresses.map(address => geocodeAddress(address, apiKey));
+    const results = await Promise.allSettled(geocodePromises);
+    const geocodedMarkers = results
+      .filter(result => result.status === 'fulfilled')
+      .map(result => result.value);
+
+      console.log("Number of markers to be placed on the map:", geocodedMarkers);
+
     return geocodedMarkers;
   } catch (error) {
     console.error("Geocoding error:", error);
-    throw error; // Re-throw to handle it in the calling component.
+    throw error;
   }
 };
 
