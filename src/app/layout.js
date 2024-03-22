94b8416bb731f5ddf841b7d3acd65fd331888889
ArturT <mr.arturio@gmail.com>
2024-03-22 00:38:33 -0400

    move navbar and footer to layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 // Initialize the font
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
