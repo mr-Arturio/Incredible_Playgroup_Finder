dc835a91695be7c0245a45ea74b42e64e9548dab
ArturT <mr.arturio@gmail.com>
2024-03-31 23:47:25 -0400

    add language filter logic + small fixes

--- src/components/RenderSheetDataTable.js
+++ src/components/RenderSheetDataTable.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from "react";
 import PlaygroupCard from "./PlaygroupCard";
-import filterByLocation from "../utils/filtering/filterByLocation";
+import applyFilters from "../utils/applyFilters";
 
 const RenderSheetDataTable = ({ sheetData }) => {
   if (!sheetData || sheetData.length === 0)
     return <div className="text-center p-4">Loading...</div>;
 
-    const [filteredData, setFilteredData] = useState(sheetData);
-    const [filterCriteria, setFilterCriteria] = useState("");
-  
-    // Defining filter options
-    const filterOptions = ["Central", "South", "East", "West"];
-  
-    useEffect(() => {
-      // Apply filtering based on location
-      let filtered = filterByLocation(sheetData, filterCriteria);
-  
-      setFilteredData(filtered);
-    }, [sheetData, filterCriteria]); // Rerun when sheetData or filterCriteria changes
+  const [filteredData, setFilteredData] = useState(sheetData);
+  const [filterCriteria, setFilterCriteria] = useState({
+    location: "",
+    language: "",
+  });
+  const [locationOptions, setLocationOptions] = useState([]);
+  const [languageOptions, setLanguageOptions] = useState([]);
+
+  useEffect(() => {
+    // Extract unique locations from sheetData
+    const uniqueLocations = new Set(
+      sheetData.map((item) => item.Location).filter(Boolean)
+    );
+    const uniqueLanguages = new Set(
+      sheetData.map((item) => item.Language).filter(Boolean)
+    );
+    setLocationOptions([...uniqueLocations]);
+    setLanguageOptions([...uniqueLanguages]);
+
+    // Apply filtering based on location
+    let filtered = applyFilters(sheetData, filterCriteria);
+    setFilteredData(filtered);
+  }, [sheetData, filterCriteria]);
 
   return (
     <div
@@ -32,13 +43,32 @@ const RenderSheetDataTable = ({ sheetData }) => {
           Filter by Location:{" "}
         </label>
         <select
-          id="filterCriteria"
-          value={filterCriteria}
-          onChange={(e) => setFilterCriteria(e.target.value)}
-          className="form-select block w-1/4 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
+          id="locationCriteria"
+          value={filterCriteria.location}
+          onChange={(e) =>
+            setFilterCriteria({ ...filterCriteria, location: e.target.value })
+          }
+          className="form-select block w-full md:w-1/4 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 mb-4 md:mb-0 md:mr-4"
         >
           <option value="">Select Location</option>
-          {filterOptions.map((option) => (
+          {locationOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+
+        {/* Language filter select */}
+        <select
+          id="languageCriteria"
+          value={filterCriteria.language}
+          onChange={(e) =>
+            setFilterCriteria({ ...filterCriteria, language: e.target.value })
+          }
+          className="form-select block w-full md:w-1/4 text-gray-700 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500"
+        >
+          <option value="">Select Language</option>
+          {languageOptions.map((option) => (
             <option key={option} value={option}>
               {option}
             </option>
