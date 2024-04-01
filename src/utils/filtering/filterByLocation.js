098742caea976113c036e428f70fcea496626a4e
ArturT <mr.arturio@gmail.com>
2024-03-31 23:47:25 -0400

    add language filter logic + small fixes

--- src/utils/filtering/filterByLocation.js
+++
@@ -1,6 +0,0 @@
-const filterByLocation = (data, location) => {
-  if (!location) return data; // Return all data if no location filter is applied
-  return data.filter(row => row.Location === location);
-};
-
-export default filterByLocation;
