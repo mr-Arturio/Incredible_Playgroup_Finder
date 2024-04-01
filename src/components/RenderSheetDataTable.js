4249a6032abfada02dcfdb702e2fdd89d00813db
ArturT <mr.arturio@gmail.com>
2024-03-31 22:21:41 -0400

    remove sorting and move filter to separate file

--- src/components/RenderSheetDataTable.js
+++ src/components/RenderSheetDataTable.js
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from "react";
 import PlaygroupCard from "./PlaygroupCard";
+import filterByLocation from "../utils/filtering/filterByLocation";
 
 const RenderSheetDataTable = ({ sheetData }) => {
   if (!sheetData || sheetData.length === 0)
     return <div className="text-center p-4">Loading...</div>;
 
-  const [filteredData, setFilteredData] = useState(sheetData);
-  const [filterCriteria, setFilterCriteria] = useState("");
-  const [sortCriteria, setSortCriteria] = useState("");
-
-  // Defining filter options
-  const filterOptions = ["Central", "South", "East", "West"];
-
-  // Use the first row of sheetData to determine headers
-  // const headers = Object.keys(sheetData[0]);
-
-  useEffect(() => {
-    // Apply filtering
-    let filtered = sheetData;
-    if (filterCriteria) {
-      filtered = sheetData.filter((row) => row.Location === filterCriteria);
-    }
-
-    // Apply sorting
-    if (sortCriteria) {
-      filtered.sort((a, b) => a[sortCriteria].localeCompare(b[sortCriteria]));
-    }
-
-    setFilteredData(filtered);
-  }, [sheetData, filterCriteria, sortCriteria]); // Rerun when sheetData, filterCriteria, or sortCriteria changes
+    const [filteredData, setFilteredData] = useState(sheetData);
+    const [filterCriteria, setFilterCriteria] = useState("");
+  
+    // Defining filter options
+    const filterOptions = ["Central", "South", "East", "West"];
+  
+    useEffect(() => {
+      // Apply filtering based on location
+      let filtered = filterByLocation(sheetData, filterCriteria);
+  
+      setFilteredData(filtered);
+    }, [sheetData, filterCriteria]); // Rerun when sheetData or filterCriteria changes
 
   return (
     <div
@@ -55,8 +44,6 @@ const RenderSheetDataTable = ({ sheetData }) => {
             </option>
           ))}
         </select>
-        {/* <button onClick={() => setSortCriteria("Location")}>Sort by Location</button> */}
-        {/* Add more sorting options as needed */}
       </div>
 
       {filteredData.map((playgroup) => (
