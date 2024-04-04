d78d8408aa334a915062882c34d7274959675d51
ArturT <mr.arturio@gmail.com>
2024-04-04 19:46:23 -0400

    create Header component and implementb it into main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
 import MapComponent from "../components/MapComponent";
 import { getSheetData } from "../actions/getSheetData";
 import RenderSheetDataTable from "../components/RenderSheetDataTable";
+import { Header } from "../components/Header";
 
 function Home() {
   const [sheetData, setSheetData] = useState(null);
@@ -20,28 +20,11 @@ function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
       {/* Top Content Area - Always present */}
-      <div className="w-10/12 p-4">
-        <div className="flex-col items-center justify-between">
-          <Link href="/charts">
-          <button className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none py-2 px-4 rounded">
-              Charts
-            </button>
-          </Link>
-          <p className="mt-8 md:mt-4 md:mb-8 text-left">
-            Ut sint laboris nostrud voluptate veniam dolore dolor duis magna ex.
-            Reprehenderit velit minim labore cillum. Ullamco ut ipsum deserunt
-            sunt. Incididunt amet nulla non elit qui mollit aliqua est Lorem
-            duis. Labore elit sint ipsum magna. Quis nulla excepteur consequat
-            duis cupidatat adipisicing eiusmod fugiat laborum in occaecat.
-            Officia dolor reprehenderit dolore aliqua esse mollit proident irure
-            laboris voluptate laboris. Laboris id amet adipisicing eu ipsum
-            commodo eiusmod est veniam proident excepteur adipisicing. Aliquip
-            Lorem id ullamco velit dolore nisi.
-          </p>
-        </div>
+      <div className=" max-w-screen-2xl w-11/12 p-4">
+        <Header />
       </div>
 
-      <main className="flex-1 w-10/12">
+      <main className="max-w-screen-2xl flex-1  w-11/12">
         {/* RenderSheetDataTable on the left (60%) */}
         <div className="flex flex-col md:flex-row justify-center">
           <div className="w-full md:w-3/5 p-4">
