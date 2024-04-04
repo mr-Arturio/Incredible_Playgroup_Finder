8e046a8eb5613c4bde8680b18b2a3d276192c7b6
ArturT <mr.arturio@gmail.com>
2024-04-04 00:32:28 -0400

    Responsive Design

--- src/app/page.js
+++ src/app/page.js
@@ -18,20 +18,40 @@ function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col bg-gray-100 min-h-screen">
-      <main className="flex flex-1">
-        <div className="flex w-full">
-          {/* RenderSheetDataTable on the left (60%) */}
-          <div className="w-3/5 p-4">
-            <div className="flex items-center justify-between">
-            <Link href="/charts">Charts</Link>
-            </div>
+    <div className="flex flex-col items-center justify-center bg-gray-100 min-h-screen">
+      {/* Top Content Area - Always present */}
+      <div className="w-10/12 p-4">
+        <div className="flex-col items-center justify-between">
+          <Link href="/charts">
+          <button className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none py-2 px-4 rounded">
+              Charts
+            </button>
+          </Link>
+          <p className="mt-8 md:mt-4 md:mb-8 text-left">
+            Ut sint laboris nostrud voluptate veniam dolore dolor duis magna ex.
+            Reprehenderit velit minim labore cillum. Ullamco ut ipsum deserunt
+            sunt. Incididunt amet nulla non elit qui mollit aliqua est Lorem
+            duis. Labore elit sint ipsum magna. Quis nulla excepteur consequat
+            duis cupidatat adipisicing eiusmod fugiat laborum in occaecat.
+            Officia dolor reprehenderit dolore aliqua esse mollit proident irure
+            laboris voluptate laboris. Laboris id amet adipisicing eu ipsum
+            commodo eiusmod est veniam proident excepteur adipisicing. Aliquip
+            Lorem id ullamco velit dolore nisi.
+          </p>
+        </div>
+      </div>
+
+      <main className="flex-1 w-10/12">
+        {/* RenderSheetDataTable on the left (60%) */}
+        <div className="flex flex-col md:flex-row justify-center">
+          <div className="w-full md:w-3/5 p-4">
+            {" "}
             <h1 className="text-lg font-semibold mb-4">The Data</h1>
             <RenderSheetDataTable sheetData={sheetData} />
           </div>
 
           {/* MapComponent on the right (40%) */}
-          <div className="w-2/5">
+          <div className="w-full md:w-2/5">
             <MapComponent sheetData={sheetData} />
           </div>
         </div>
