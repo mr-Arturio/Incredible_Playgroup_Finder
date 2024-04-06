387926363cf728331e1cc21f8982500dbf2023ce
ArturT <mr.arturio@gmail.com>
2024-04-05 20:49:21 -0400

    fix React hooks call order

--- src/components/RenderSheetDataTable.js
+++ src/components/RenderSheetDataTable.js
@@ -5,12 +5,8 @@ import FilterComponent from "./FilterComponent";
 import MapComponent from "./MapComponent";
 
 const RenderSheetDataTable = ({ sheetData }) => {
+  const isLoading = !sheetData || sheetData.length === 0;
 
-      // Check for sheetData availability after all hook calls
-      if (!sheetData || sheetData.length === 0) 
-        return <div className="text-center p-4">Loading...</div>;
-      
-    
   const [filteredData, setFilteredData] = useState(sheetData);
   const [filterCriteria, setFilterCriteria] = useState({
     location: "",
@@ -29,78 +25,77 @@ const RenderSheetDataTable = ({ sheetData }) => {
     "Friday",
     "Saturday",
   ]);
-  
 
   useEffect(() => {
-    // Extract unique locations from sheetData
-    const uniqueLocations = new Set(
-      sheetData.map((item) => item.Location).filter(Boolean)
-    );
-    const uniqueLanguages = new Set(
-      sheetData.map((item) => item.Language).filter(Boolean)
-    );
-    const uniqueNames = new Set(
-      sheetData.map((item) => item.Name).filter(Boolean)
-    );
+    if (!isLoading) {
+      // Extract unique locations from sheetData
+      const uniqueLocations = new Set(
+        sheetData.map((item) => item.Location).filter(Boolean)
+      );
+      const uniqueLanguages = new Set(
+        sheetData.map((item) => item.Language).filter(Boolean)
+      );
+      const uniqueNames = new Set(
+        sheetData.map((item) => item.Name).filter(Boolean)
+      );
 
-    setLocationOptions([...uniqueLocations]);
-    setLanguageOptions([...uniqueLanguages]);
-    setNameOptions([...uniqueNames]);
+      setLocationOptions([...uniqueLocations]);
+      setLanguageOptions([...uniqueLanguages]);
+      setNameOptions([...uniqueNames]);
 
-    // Apply filtering based on location
-    let filtered = applyFilters(sheetData, filterCriteria);
-    setFilteredData(filtered);
-  }, [sheetData, filterCriteria]);
+      // Apply filtering based on location
+      let filtered = applyFilters(sheetData, filterCriteria);
+      setFilteredData(filtered);
+    }
+  }, [sheetData, filterCriteria, isLoading]);
+
+  if (isLoading) return <div className="text-center p-4">Loading...</div>;
 
   const handleFilterChange = (key, value) => {
     setFilterCriteria({ ...filterCriteria, [key]: value });
   };
-
-
 
   return (
     <div className="flex flex-col md:flex-col">
       {/* Filter Components */}
       <div className="sticky top-0 bg-white z-10 p-4 w-full">
         <div className="flex flex-wrap justify-between items-center">
-        <FilterComponent
-                id="nameCriteria"
-                value={filterCriteria.name}
-                options={nameOptions}
-                onChange={(e) => handleFilterChange("name", e.target.value)}
-                placeholder="Select Facility"
-              />
+          <FilterComponent
+            id="nameCriteria"
+            value={filterCriteria.name}
+            options={nameOptions}
+            onChange={(e) => handleFilterChange("name", e.target.value)}
+            placeholder="Select Facility"
+          />
 
-              <FilterComponent
-                id="locationCriteria"
-                value={filterCriteria.location}
-                options={locationOptions}
-                onChange={(e) => handleFilterChange("location", e.target.value)}
-                placeholder="Select Location"
-              />
+          <FilterComponent
+            id="locationCriteria"
+            value={filterCriteria.location}
+            options={locationOptions}
+            onChange={(e) => handleFilterChange("location", e.target.value)}
+            placeholder="Select Location"
+          />
 
-              <FilterComponent
-                id="languageCriteria"
-                value={filterCriteria.language}
-                options={languageOptions}
-                onChange={(e) => handleFilterChange("language", e.target.value)}
-                placeholder="Select Language"
-              />
+          <FilterComponent
+            id="languageCriteria"
+            value={filterCriteria.language}
+            options={languageOptions}
+            onChange={(e) => handleFilterChange("language", e.target.value)}
+            placeholder="Select Language"
+          />
 
-              <FilterComponent
-                id="dayCriteria"
-                value={filterCriteria.day}
-                options={dayOptions}
-                onChange={(e) => handleFilterChange("day", e.target.value)}
-                placeholder="Select Day of the Week"
-              />
+          <FilterComponent
+            id="dayCriteria"
+            value={filterCriteria.day}
+            options={dayOptions}
+            onChange={(e) => handleFilterChange("day", e.target.value)}
+            placeholder="Select Day of the Week"
+          />
         </div>
       </div>
 
-
-
-   {/* Content Sections */}
-   <div className="flex flex-1 md:flex-row">
+      {/* Content Sections */}
+      <div className="flex flex-1 md:flex-row">
         {/* Playgroup Cards Section */}
         <div className="w-full md:w-3/5 p-4">
           <div
