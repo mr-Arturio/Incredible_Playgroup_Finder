9245e5ae7fdf73f54721e6f0330a0d0acee7488e
ArturT <mr.arturio@gmail.com>
2024-04-18 01:20:55 -0400

    add basic text and image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Header = () => {
   return (
     <div className="flex-col items-center justify-between">
-      <Link href="/charts">
-        <button className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none py-2 px-4 rounded">
-          Charts
-        </button>
-      </Link>
-      <h1 className="text-4xl font-bold text-center mt-4">
+      <div className="flex flex-col items-center justify-between md:flex-row md:items-center">
+      <Image
+          src="/Play_Goup.png"
+          alt="Play Group"
+          width={200}
+          height={200}
+        />
+              <h1 className="text-4xl font-bold text-center mt-4">
         Welcome to the Incredible Playgroup Finder!
       </h1>
+        <Link href="/charts">
+          <button className="text-white bg-blue-500 hover:bg-blue-600 focus:outline-none py-2 px-4 rounded">
+            Charts
+          </button>
+        </Link>
+
+      </div>
+
       <p className="mt-8 md:mt-4 md:mb-8 text-left">
         Conceived and created by an Ottawa Mom, Ottawa parents now have a new,
         incredibly useful tool to plan EarlyON playgroup outings with their
