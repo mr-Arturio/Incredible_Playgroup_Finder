9c8b0324a8b9236a073415e38d397b22c07fbcaf
ArturT <mr.arturio@gmail.com>
2024-05-13 19:19:01 -0400

    uncoment cache logic + add try/catch

--- src/actions/getSheetData.js
+++ src/actions/getSheetData.js
@@ -22,35 +22,44 @@ export async function getSheetData() {
     console.log("Cache miss: Fetching new data");
   }
 
-  const glAuth = await google.auth.getClient({
-    projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
-    credentials: {
-      type: "service_account",
-      private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
-      client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+  try {
+    const glAuth = await google.auth.getClient({
+      projectId: process.env.GOOGLE_SHEETS_PROJECT_ID,
+      credentials: {
+        type: "service_account",
+        private_key: process.env.GOOGLE_SHEETS_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
 
-  const glSheets = google.sheets({ version: "v4", auth: glAuth });
+    const glSheets = google.sheets({ version: "v4", auth: glAuth });
 
-  const data = await glSheets.spreadsheets.values.get({
-    spreadsheetId: process.env.GOOGLE_SHEET_ID,
-    range: "A:T",
-  });
+    const data = await glSheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "A:T",
+    });
 
-  const transformedData = transformDataToObjects(data.data.values);
+    const transformedData = transformDataToObjects(data.data.values);
 
-  // if (transformedData) {
-  //   // Only cache if data is not null
-  //   sheetCache.set(cacheKey, transformedData);
-  //   console.log("New data cached");
-  //   // console.log(transformedData);
-  // }
+    if (transformedData) {
+      // Only cache if data is not null
+      sheetCache.set(cacheKey, transformedData);
+      console.log("New data cached");
+      // console.log(transformedData);
+    }
 
-  return {
-    props: {
-      sheetData: transformedData, // Now returns data as objects
-    },
-  };
+    return {
+      props: {
+        sheetData: transformedData, // Now returns data as objects
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching sheet data:", error);
+    return {
+      props: {
+        sheetData: null,
+      },
+    };
+  }
 }
