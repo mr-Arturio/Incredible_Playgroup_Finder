94f7253d49f56102b1ccb58490d72c394958ab1f
ArturT <mr.arturio@gmail.com>
2024-05-12 22:30:44 -0400

    create contact form file

---
+++ src/components/ContactForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactForm() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLScriz1W2G8caKuIYcnJtb-V_j4JzNgFmyDOoULg0NDZHicoHg/viewform?usp=pp_url"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Contact Us
+      </a>
+    </div>
+  );
+}
+
+export default ContactForm;
