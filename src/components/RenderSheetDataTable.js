04cef3e55ac3206de17fc465ce78819ffe0981cf
ArturT <mr.arturio@gmail.com>
2024-07-02 22:35:35 -0400

    work on active filter list, improve mobile layout, move styling to the correct components

--- src/components/RenderSheetDataTable.js
+++ src/components/RenderSheetDataTable.js
@@ -141,11 +141,7 @@ const RenderSheetDataTable = ({ sheetData }) => {
         />
       </div>
       {/* Filters container */}
-      <div
-        className={`flex flex-col py-3 px-5 space-y-4 lg:flex-row lg:space-y-0 lg:space-x-4 bg-white rounded-lg shadow mb-4 w-full ${
-          isFilterVisible ? "" : "hidden md:flex"
-        }`}
-      >
+      <div className={` ${isFilterVisible ? "" : "hidden md:flex"}`}>
         <FilterContainer
           filterCriteria={filterCriteria}
           setFilterCriteria={setFilterCriteria}
