4e76a39390f04954d53ee988be9b57b284f80577
ArturT <mr.arturio@gmail.com>
2024-07-09 11:30:55 -0400

    update icons route

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,42 +1,41 @@
-
-const getIcons = (Parking, Coffee, WiFi, Outdoor, Language, Scale) => ({
-  time: "time.svg",
-  location: "location.svg",
-  age: "age.svg",
+const getIcons = (Parking, Coffee, WiFi, Outdoor, Language, Scale, lng) => ({
+  time: `/time.svg`,
+  location: `/location.svg`,
+  age: `/age.svg`,
   parking: {
     show: Parking === "Yes",
-    src: "parking.svg",
+    src: "/parking.svg",
     tooltip: "Parking Available",
   },
   coffee: {
     show: Coffee === "Yes",
-    src: "coffee.svg",
+    src: `/coffee.svg`,
     tooltip: "Coffee",
   },
   // wifi: {
   //   show: WiFi === "Yes",
-  //   src: "wifi.svg",
+  //   src: `/${lng}/wifi.svg`,
   //   tooltip: "WiFi Available",
   // },
   indoor: {
     show: Outdoor === "No",
-    src: "indoor.svg",
+    src: `/indoor.svg`,
     tooltip: "Indoor",
   },
   outdoor: {
     show: Outdoor === "Yes",
-    src: "outdoor.svg",
+    src: `/outdoor.svg`,
     tooltip: "Outdoor",
   },
   scale: {
     show: Scale === "Yes",
-    src: "scale.svg",
+    src: `/scale.svg`,
     tooltip: "Baby Scale",
   },
   // Assuming Language should always be shown; adjust if needed
   language: {
     show: true,
-    src: `${Language.toLowerCase()}.svg`, //// Dynamic path to your language icon
+    src: `${Language.toLowerCase()}.svg`, // Dynamic path to your language icon
     tooltip: Language,
   },
 });
