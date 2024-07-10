c099aa34b7e4f101612a212c1b98765d2acb26d1
ArturT <mr.arturio@gmail.com>
2024-07-10 17:39:07 -0400

    organize and group translation forms and apply

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -8,15 +8,15 @@ function ContactForm({ lng }) {
 
   return (
     <div className="flex flex-col items-start justify-center bg-gray-100 py-4">
-      <h2 className="text-2xl font-bold my-4">{t("needHelp")}</h2>
-      <p className="text-lg text-center mb-4">{t("contactForm")}</p>
+      <h2 className="text-2xl font-bold my-4">{t("contactForm.needHelp")}</h2>
+      <p className="text-lg text-center mb-4">{t("contactForm.message")}</p>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLScriz1W2G8caKuIYcnJtb-V_j4JzNgFmyDOoULg0NDZHicoHg/viewform?usp=pp_url"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-mainBlue hover:bg-hoverBlue text-white font-bold py-2 px-4 rounded-lg"
       >
-        {t("buttonText")}
+        {t("contactForm.contactUs")}
       </a>
     </div>
   );
