12bc423fff69e028dbb311739b56155e72eb19c0
ArturT <mr.arturio@gmail.com>
2024-07-09 23:28:24 -0400

    implement propper language options - using client i18n file!

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { useTranslation } from "../app/i18n";
+"use client";
 
-async function ContactForm({ lng }) {
-  const { t } = await useTranslation(lng);
+import React from "react";
+import { useTranslation } from "../app/i18n/client";
+
+function ContactForm({ lng }) {
+  const { t } = useTranslation(lng, "translation");
 
   return (
     <div className="flex flex-col items-start justify-center bg-gray-100 py-4">
-      <h2 className="text-2xl font-bold mt-4"> {t("needHelp")} </h2>
-      <p className="text-lg text-center mb-4">
-        {t("contactForm")}
-      </p>
+      <h2 className="text-2xl font-bold my-4">{t("needHelp")}</h2>
+      <p className="text-lg text-center mb-4">{t("contactForm")}</p>
       <a
         href="https://docs.google.com/forms/d/e/1FAIpQLScriz1W2G8caKuIYcnJtb-V_j4JzNgFmyDOoULg0NDZHicoHg/viewform?usp=pp_url"
         target="_blank"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import LanguageSwitcher from "./LanguageSwitcher";
-import { useTranslation } from "../app/i18n";
+import { useTranslation } from "../app/i18n/client";
+import { useEffect, useState } from "react";
 
+function Navbar({ lng }) {
+  const { t, i18n } = useTranslation(lng);
+  const [loading, setLoading] = useState(true);
 
-async function Navbar({ lng }){
-  const { t } = await useTranslation(lng);
+  useEffect(() => {
+    const changeLanguage = async () => {
+      await i18n.changeLanguage(lng);
+      setLoading(false);
+    };
+    changeLanguage();
+  }, [lng, i18n]);
+
+  if (loading) return null;
+
   return (
     <nav className="bg-plum text-white py-2">
       <div className="container mx-auto flex justify-between items-center">
         <div className="logo">
-          {/* Assuming you have a logo.png in your public folder */}
           <Image
             src="/Play_Goup.png"
             alt="Logo"
@@ -22,11 +35,11 @@ async function Navbar({ lng }){
           {t("welcome")}
         </h1>
         <div>
-        <LanguageSwitcher />
+          <LanguageSwitcher />
         </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
