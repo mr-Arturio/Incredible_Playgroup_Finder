ae7d821347f20f171978aa046f4341225df86404
ArturT <mr.arturio@gmail.com>
2024-07-11 01:03:34 -0400

    move file

---
+++ src/app/[lng]/charts/page.js
@@ -0,0 +1,9 @@
+export default function Charts() {
+  return (
+    <div className="flex items-center justify-center bg-gray-100 min-h-screen">
+      <main>
+        <p className="text-xl"><strong>Coming soon...</strong></p>
+      </main>
+    </div>
+  );
+}
