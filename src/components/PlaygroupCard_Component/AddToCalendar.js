4a0e9c8d49ee3e54c2b4d7756864ccc8f0be70d3
ArturT <mr.arturio@gmail.com>
2024-07-16 23:23:14 -0400

    Function to update the button size based on window width

--- src/components/PlaygroupCard_Component/AddToCalendar.js
+++ src/components/PlaygroupCard_Component/AddToCalendar.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 const AddToCalendar = ({ name, address, date, startTime, endTime, Cancelled }) => {
+  const [buttonSize, setButtonSize] = useState("1");
+
+  useEffect(() => {
+    // Function to update the button size based on window width
+    const updateButtonSize = () => {
+      if (window.innerWidth < 640) {
+        setButtonSize("0"); // small screens
+      } else {
+        setButtonSize("1"); // larger screens
+      }
+    };
+
+    // Initial call to set the button size
+    updateButtonSize();
+
+    // Event listener for window resize
+    window.addEventListener("resize", updateButtonSize);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("resize", updateButtonSize);
+  }, []);
+
   if (Cancelled === "Yes") return null;
 
   return (
-   
-      <AddToCalendarButton
-        name={name}
-        options={["Google", "Outlook.com", "Apple"]}
-        location={address}
-        startDate={date}
-        endDate={date}
-        startTime={startTime}
-        endTime={endTime}
-        timeZone="Canada/Eastern"
-        size="1"
-        listStyle="modal"
-        buttonStyle="text"
-        trigger="click"
-      />
-    
+    <AddToCalendarButton
+      name={name}
+      options={["Google", "Outlook.com", "Apple"]}
+      location={address}
+      startDate={date}
+      endDate={date}
+      startTime={startTime}
+      endTime={endTime}
+      timeZone="Canada/Eastern"
+      size={buttonSize}
+      listStyle="modal"
+      buttonStyle="text"
+      trigger="click"
+    />
   );
 };
 
