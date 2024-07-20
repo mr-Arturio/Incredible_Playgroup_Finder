c01daa3cf0d1908161275b0aa03546c67a19f676
ArturT <mr.arturio@gmail.com>
2024-07-19 23:27:22 -0400

    implement localStorage to keep languge after reloading

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -1,13 +1,27 @@
 'use client';
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const LanguageContext = createContext();
 
 export const useLanguage = () => useContext(LanguageContext);
 
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = useState('en');
+  // Initialize state with a function to avoid accessing localStorage directly
+  const [language, setLanguage] = useState(() => {
+    if (typeof window !== 'undefined') {
+      // Ensure we're running in the browser
+      return localStorage.getItem('language') || 'en';
+    }
+    return 'en'; // Default to 'en' if window is not defined
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Ensure we're running in the browser before accessing localStorage
+      localStorage.setItem('language', language);
+    }
+  }, [language]);
 
   const toggleLanguage = (lang) => {
     setLanguage(lang);
