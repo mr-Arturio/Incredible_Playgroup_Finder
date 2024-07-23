7c3633aedcd9312f9b616f47b38ea8a14b27d186
ArturT <mr.arturio@gmail.com>
2024-07-23 14:30:32 -0400

    implement french

--- src/components/NoDataText.js
+++ src/components/NoDataText.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import { useLanguage } from "../context/LanguageContext";
 
 const NoDataText = () => {
+  const { language } = useLanguage();
+
+  const translations = {
+    noData:
+      language === "fr"
+        ? "Aucune donnée n'a été trouvée pour les filtres sélectionnés. Veuillez ajuster vos critères de recherche."
+        : "No data was found for the selected filters. Please adjust your search criteria.",
+  };
+
   return (
     <div className="flex justify-center items-center h-full">
-    <span className="text-gray-500 text-center">
-      No data found for the selected filters. Please adjust your
-      search criteria.
-    </span>
-  </div>
+      <span className="text-gray-500 text-center">{translations.noData}</span>
+    </div>
   );
-}
+};
 
 export default NoDataText;
