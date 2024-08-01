5c6cc45eb66b8ba67c1c374f72c4e98d4b09b326
ArturT <mr.arturio@gmail.com>
2024-08-01 00:44:48 -0400

    add ugly baner svg img instead of header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
-// import LanguageSwitcher from "./LanguageSwitcher";
+// import Image from "next/image";
 import "../app/fonts.css";
-import { useLanguage } from "../context/LanguageContext";
+// import { useLanguage } from "../context/LanguageContext";
 import { useState, useEffect } from "react";
 
 const Navbar = () => {
-  const { translation } = useLanguage();
+  // const { translation } = useLanguage();
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -32,24 +31,28 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="bg-gradient-to-r from-gradient1  to-gradient2 text-white py-2">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex-1 text-center ml-2">
-          <h1 className="lg:ml-10 text-3xl 2xl:text-7xl lg:text-6xl md:text-5xl font-lazydog">
-            {translations.header[translation]}
-          </h1>
-        </div>
-        <div className="shrink ml-4">
-          <Image
-            src="/IPFicon.svg"
-            alt="Logo"
-            width={170}
-            height={170}
-                />
-        </div>
-      </div>
-      {/* <LanguageSwitcher /> */}
-    </nav>
+    <nav
+      className="h-24 md:h-52 2xl:h-96 xl:h-80 lg:h-72 bg-cover bg-center"
+      style={{ backgroundImage: "url('/NavImg.svg')" }}
+    ></nav>
+
+    // <nav className="bg-gradient-to-r from-gradient1  to-gradient2 text-white py-2">
+    // <div className="container mx-auto flex justify-between items-center">
+    //   <div className="flex-1 text-center ml-2">
+    //     <h1 className="lg:ml-10 text-3xl 2xl:text-7xl lg:text-6xl md:text-5xl font-lazydog">
+    //       {translations.header[translation]}
+    //     </h1>
+    //   </div>
+    //   <div className="shrink ml-4">
+    //     <Image
+    //       src="/IPFicon.svg"
+    //       alt="Logo"
+    //       width={170}
+    //       height={170}
+    //           />
+    //   </div>
+    // </div>
+    // </nav>
   );
 };
 
