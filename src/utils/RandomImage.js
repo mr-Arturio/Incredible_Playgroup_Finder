f1b66120494e6aa229104facabb89a2b4637e031
ArturT <mr.arturio@gmail.com>
2024-08-06 00:29:33 -0400

    fix, return useState

--- src/utils/RandomImage.js
+++ src/utils/RandomImage.js
@@ -1,18 +1,16 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 const RandomImage = () => {
-  // Generate a random number between 1 and 16
-  const randomNumber = Math.floor(Math.random() * 16) + 1;
-  // Set the image source based on the random number
-  const randomImage = `/docs/Families/${randomNumber}.svg`;
+  const [randomImage, setRandomImage] = useState("");
+
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 12) + 1;
+    setRandomImage(`/docs/Families/${randomNumber}.svg`);
+  }, []);
 
   return (
-    <Image
-      src={randomImage}
-      alt={`Family Image ${randomNumber}`}
-      width={370}
-      height={370}
-    />
+    <Image src={randomImage} alt="Family Image" width={370} height={370} />
   );
 };
 
