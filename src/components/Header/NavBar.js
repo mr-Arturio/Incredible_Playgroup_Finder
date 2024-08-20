234ece594c437dee54a082c6c25be207bbba8cde
ArturT <mr.arturio@gmail.com>
2024-08-19 23:14:11 -0400

    adjust tablet view

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
 
   return (
     <nav className="bg-white shadow-lg">
-      <div className="container mx-auto 2xl:px-16 flex justify-between items-center py-2.5 px-2.5">
+      <div className="container mx-auto 2xl:px-16 flex justify-between items-center py-2.5 lg:px-2.5">
         {/* Logo Section */}
         <div className="flex items-center">
           <a
@@ -20,7 +20,7 @@ const NavBar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src="/logo_english.svg" alt="Logo" className="h-14 md:h-20 w-auto" />
+            <img src="/logo_english.svg" alt="Logo" className="h-14 lg:h-16 xl:h-20 w-auto" />
           </a>
         </div>
 
@@ -36,7 +36,7 @@ const NavBar = () => {
         </div>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex 2xl:space-x-6 space-x-3 mr-8">
+        <div className="hidden md:flex 2xl:space-x-6 xl:space-x-3 xl:mr-8">
           <Link href="https://www.parentresource.ca/" className="navBarText">
             Home
           </Link>
@@ -78,7 +78,7 @@ const NavBar = () => {
           </Link>
         </div>
         {/* Social Icons and Button */}
-        <div className="hidden md:flex items-center space-x-3">
+        <div className="hidden lg:flex items-center xl:space-x-3 space-x-1">
           <a
             href="https://www.facebook.com/parentresourcecentre"
             target="_blank"
@@ -87,7 +87,7 @@ const NavBar = () => {
             <img
               src="https://static.wixstatic.com/media/11062b_0bec1cadb27b4d4a9898a740648fc5a9~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_0bec1cadb27b4d4a9898a740648fc5a9~mv2.png"
               alt="Facebook"
-              className="h-[37px] w-[37px] object-cover hover:scale-110"
+              className="navBarIcons"
             />
           </a>
           <a
@@ -98,7 +98,7 @@ const NavBar = () => {
             <img
               src="https://static.wixstatic.com/media/11062b_482d38aa2aaa49a5b45774ebe9a5b544~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_482d38aa2aaa49a5b45774ebe9a5b544~mv2.png"
               alt="Instagram"
-              className="h-[37px] w-[37px] object-cover hover:scale-110"
+              className="navBarIcons"
             />
           </a>
           <a
@@ -109,16 +109,16 @@ const NavBar = () => {
             <img
               src="https://static.wixstatic.com/media/11062b_23e5890c2dfc4a04af80178b43ef66fd~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_23e5890c2dfc4a04af80178b43ef66fd~mv2.png"
               alt="LinkedIn"
-              className="h-[37px] w-[37px] object-cover hover:scale-110"
+              className="navBarIcons"
             />
           </a>
         </div>
-        <div className="hidden md:flex">
+        <div className="hidden lg:flex">
           <a
             href="https://www.canadahelps.org/en/dn/27631"
             target="_blank"
             rel="noopener noreferrer"
-            className="relative bg-mainBlue text-white border-transparent border-solid border-0 rounded-lg shadow-lg px-5 py-3 hover:bg-amber transition-colors duration-500 ease-in-out flex items-center justify-center"
+            className="text-sm relative bg-mainBlue text-white border-transparent border-solid border-0 rounded-lg shadow-lg px-2 xl:px-5 py-1 xl:py-3 hover:bg-amber transition-colors duration-500 ease-in-out flex items-center justify-center"
           >
             Donate Now
           </a>
