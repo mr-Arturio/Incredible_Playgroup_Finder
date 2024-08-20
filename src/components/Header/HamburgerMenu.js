2a8acfd3aa901e708715e342ab841cb089708653
ArturT <mr.arturio@gmail.com>
2024-08-19 22:52:43 -0400

    create hum meny component

---
+++ src/components/Header/HamburgerMenu.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { FiX } from "react-icons/fi";
+
+const HamburgerMenu = ({ isOpen, toggleMenu }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-purple-800 z-50 flex flex-col items-center justify-center">
+      <button onClick={toggleMenu} className="absolute top-5 right-5">
+        <FiX className="text-white text-4xl" />
+      </button>
+      <div className="flex flex-col space-y-6 text-white text-2xl">
+        <Link href="https://www.parentresource.ca/" onClick={toggleMenu}>
+          Home
+        </Link>
+        <Link href="https://www.parentresource.ca/about" onClick={toggleMenu}>
+          About Us
+        </Link>
+        <Link href="https://www.parentresource.ca/copy-of-our-impact" onClick={toggleMenu}>
+          Get Involved
+        </Link>
+        <Link href="https://www.parentresource.ca/programs" onClick={toggleMenu}>
+          Parents
+        </Link>
+        <Link href="https://www.parentresource.ca/professionals" onClick={toggleMenu}>
+          Professionals
+        </Link>
+        <Link href="https://www.parentresource.ca/blog" onClick={toggleMenu}>
+          Blog
+        </Link>
+        <Link href="https://www.parentresource.ca/volunteering" onClick={toggleMenu}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
