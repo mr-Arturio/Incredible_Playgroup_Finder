75b5b5b87d7f849d891af5d735cb794a3322b264
ArturT <mr.arturio@gmail.com>
2024-08-26 11:51:54 -0400

    fix filtering in french language

---
+++ src/utils/translationMappings.js
@@ -0,0 +1,37 @@
+export const translationMappings = {
+  fr: {
+
+    "Lundi": "Monday",
+    "Mardi": "Tuesday",
+    "Mercredi": "Wednesday",
+    "Jeudi": "Thursday",
+    "Vendredi": "Friday",
+    "Samedi": "Saturday",
+    "Dimanche": "Sunday",
+
+    "Matin": "Morning",
+    "Après-midi": "Afternoon",
+    "Soir": "Evening",
+
+    "Est": "East",
+    "Ouest": "West",
+    "Centre": "Central",
+    "Sud": "South",
+
+    "Bébé (non marchant)": "Baby (non-walking)",
+    "Bébé (0-12mois)": "Baby (0-12m)",
+    "Bébé (0-18mois)": "Baby (0-18m)",
+    "Bébé (0-24mois)": "Baby (0-24m)",
+    "Enfant (0-6ans)": "Child (0-6y)",
+    "Enfant (3-6ans)": "Child (3-6y)",
+    "Enfant (4-10ans)": "Child (4-10y)",
+
+    "Anglais": "English",
+    "Français": "French",
+    "Anglais/Français": "English/French",
+    "L'arabe": "Arabic",
+
+    // Day mapping located in Activefilters file
+
+  }
+};
