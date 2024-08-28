112efd20ab560cfaa4bab7168f027d8e32af1d78
ArturT <mr.arturio@gmail.com>
2024-08-27 23:59:24 -0400

    implement Image instead of img

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image"; // Import the Image component
 import { FiMenu, FiX } from "react-icons/fi";
 import HamburgerMenu from "./HamburgerMenu";
 import { useLanguage } from "../../context/LanguageContext";
@@ -16,7 +17,7 @@ const NavBar = () => {
 
   return (
     <nav className="bg-white shadow-lg">
-      <div className="container mx-auto 2xl:px-16 flex justify-between items-center py-2 sm:py-2.5  px-7 sm:px-4 lg:px-2.5 ">
+      <div className="container mx-auto 2xl:px-16 flex justify-between items-center py-2 sm:py-2.5 px-7 sm:px-4 lg:px-2.5 ">
         {/* Logo Section */}
         <div className="flex items-center">
           <a
@@ -24,10 +25,15 @@ const NavBar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={icon} alt="Logo" className="h-14 lg:h-16 xl:h-20 w-auto" />
+            <Image
+              src={icon}
+              alt="Logo"
+              width={100}
+              height={100}
+              className="h-14 lg:h-16 xl:h-20 w-auto"
+            />
           </a>
         </div>
-
         {/* Hamburger Icon for Mobile */}
         <div className="md:hidden flex items-center">
           <button onClick={toggleMenu}>
@@ -38,7 +44,6 @@ const NavBar = () => {
             )}
           </button>
         </div>
-
         {/* Navigation Links */}
         <div className="hidden md:flex 2xl:space-x-6 xl:space-x-3 xl:mr-8">
           <Link href="https://www.parentresource.ca/" className="navBarText">
@@ -88,9 +93,11 @@ const NavBar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="https://static.wixstatic.com/media/11062b_0bec1cadb27b4d4a9898a740648fc5a9~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_0bec1cadb27b4d4a9898a740648fc5a9~mv2.png"
               alt="Facebook"
+              width={39}
+              height={39}
               className="navBarIcons"
             />
           </a>
@@ -99,9 +106,11 @@ const NavBar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="https://static.wixstatic.com/media/11062b_482d38aa2aaa49a5b45774ebe9a5b544~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_482d38aa2aaa49a5b45774ebe9a5b544~mv2.png"
               alt="Instagram"
+              width={39}
+              height={39}
               className="navBarIcons"
             />
           </a>
@@ -110,9 +119,11 @@ const NavBar = () => {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
+            <Image
               src="https://static.wixstatic.com/media/11062b_23e5890c2dfc4a04af80178b43ef66fd~mv2.png/v1/fill/w_39,h_39,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_23e5890c2dfc4a04af80178b43ef66fd~mv2.png"
               alt="LinkedIn"
+              width={39}
+              height={39}
               className="navBarIcons"
             />
           </a>
