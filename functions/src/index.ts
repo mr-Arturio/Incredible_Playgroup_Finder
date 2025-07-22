e87c8aeedf40ba7d7971f3081d5b5e228f042895
mr-Arturio <mr.arturio@gmail.com>
2025-07-22 01:27:31 -0400

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,8 +8,8 @@
  */
 
 import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+// import {onRequest} from "firebase-functions/https";
+// import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
