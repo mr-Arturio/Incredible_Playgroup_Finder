faf53a75d539f7bac159345fed57eae2d3c1b891
mr-Arturio <artur.tereshchenko@mrge.com>
2025-12-05 21:37:21 -0500

    holiday campain nbanner

--- src/components/PromoLink.js
+++ src/components/PromoLink.js
@@ -4,22 +4,22 @@ import Tooltip from "../utils/Tooltip";
 
 const PromoLink = ({
   tooltipClassName = "bg-gray-100/25 text-sm font-sans border border-gray-300 shadow-md hidden md:flex",
-  imageClassName = "hidden md:flex w-96 h-auto cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 rounded-lg border border-mainBlue hover:border-hoverBlue shadow-sm hover:shadow-lg",
-  mobileTooltipClassName ="bg-gray-200 text-sm font-sans border border-gray-300 shadow-md flex md:hidden",
+  imageClassName = "hidden md:flex w-[36rem] h-auto cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 rounded-lg border border-mainBlue hover:border-hoverBlue shadow-sm hover:shadow-lg",
+  mobileTooltipClassName = "bg-gray-200 text-sm font-sans border border-gray-300 shadow-md flex md:hidden",
   mobileTextClassName = "md:hidden flex font-bold text-md sm:text-lg text-introText tracking-wider cursor-pointer drop-shadow-[0_0_10px_rgba(255,105,180,0.6)] transition duration-300 hover:drop-shadow-[0_0_20px_rgba(255,105,180,0.9)] hover:scale-105 text-wrap",
 }) => {
   const handleClick = () => {
-    track("Countdown to Kindergarten", { label: "PRC Virtual Workshop - July 16" });
+    track("50 years donation", { label: "PRC 50 years donation" });
   };
 
-  const eventUrl = "https://www.eventbrite.ca/e/countdown-to-kindergarten-tickets-1405819998459?aff=oddtdtcreator";
+  const eventUrl = "https://www.parentresource.ca/holidaycampaign";
 
   return (
     <>
       {/* Desktop */}
       {imageClassName !== "hidden" && (
         <Tooltip
-          text="July 16, 6:00pm. By Ottawa Parent Resource Centre."
+          text="Donate to PRC for 50 years of support"
           className={tooltipClassName}
         >
           <a
@@ -29,8 +29,8 @@ const PromoLink = ({
             onClick={handleClick}
           >
             <img
-              src="/Countdown_to_Kindergarten.png"
-              alt="Countdown to Kindergarten Workshop Banner"
+              src="/banner/50_years.svg"
+              alt="50 years donation banner"
               className={imageClassName}
             />
           </a>
@@ -40,8 +40,8 @@ const PromoLink = ({
       {/* Mobile */}
       {mobileTextClassName !== "hidden" && (
         <Tooltip
-        text="PRC Virtual Workshop - July 16"
-        className={mobileTooltipClassName}
+          text="Donate to Holiday Campaign"
+          className={mobileTooltipClassName}
         >
           <a
             href={eventUrl}
@@ -50,7 +50,7 @@ const PromoLink = ({
             onClick={handleClick}
             className={mobileTextClassName}
           >
-            Countdown to Kindergarten
+            Donate to Holiday Campaign
           </a>
         </Tooltip>
       )}
